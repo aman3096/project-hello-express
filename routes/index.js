2e45f5aa5fd31f8c5efd421619cd03b32bd7e9c8
Aman Tandon <aman.tandon3096@gmail.com>
2024-11-03 00:54:49 +0530

    Adding secure route

--- routes/index.js
+++ routes/index.js
@@ -66,4 +66,25 @@ router.post('/auth/register', async (req,res) => {
   }
 
 })
+
+router.get("/super-secure-resource", (req, res) => {
+  if (!req.headers.authorization) {
+    return res.status(401).json({ error: "Not Authorized" });
+  }
+
+  // Bearer <token>>
+  const authHeader = req.headers.authorization;
+  const token = authHeader.split(" ")[1];
+
+  try {
+    // Verify the token is valid
+    const { user } = jsonwebtoken.verify(token, process.env.JWT_SECRET);
+    return res.status(200).json({
+      message: `Congrats ${user}! You can now accesss the super secret resource`,
+    });
+  } catch (error) {
+    return res.status(401).json({ error: "Not Authorized" });
+  }
+});
+
 module.exports = router;
