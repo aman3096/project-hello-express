493e5e5d8665aea64771ee101bb010e42ac7a0ab
Aman Tandon <aman.tandon3096@gmail.com>
2024-11-06 19:51:21 +0530

    feat: Adding post and put to account api

--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 const { Pool } = require('pg');
 
 const authenticate = require('../middlewares/authenticate');
@@ -35,4 +36,58 @@ router.get('/', authenticate, async (req,res)=> {
         return res.status("Server error occurred");
     }
 })
+
+router.post('/', authenticate, async (req, res) => {
+    const { name, type, initialBal } = req.body
+    const random_uuid = uuidv4();
+    try{
+
+        if(!name.length || !type.length || !initialBal.length) {
+            res.send("Please do not provide empty entries")
+        }
+        const query = "INSERT id, name, balance into accounts VALUES ($1, $2, $3, $4)";
+        const values = [random_uuid, name, type, initialBal]
+        await pool.query(query, values);
+        const fetchQuery = "SELECT id, name, balance FROM accounts WHERE name=$1"
+        const fetchVals = [name];
+        const result = await pool.query(fetchQuery, fetchVals);
+        const output = {
+            message: "Account added successfully",
+            data: result.rows
+        } 
+        res.send(output);
+    } catch(err) {
+        console.error(err);
+        res.send("Server Error Occurred");
+    }
+})
+
+
+router.put('/', authenticate, async (req, res) => {
+    const { name, type, initialBal } = req.body
+    try{
+
+        if(!name.length || !type.length || !initialBal.length) {
+            res.send("Please do not provide empty entries")
+        }
+        if(type!='Account owner') {
+            res.send('No rights found for the users');
+        }
+        const query = "UPDATE accounts SET name=$1, type=$2, balance=$3)";
+        const values = [name, type, initialBal]
+        await pool.query(query, values);
+        const fetchQuery = "SELECT id, name, balance FROM accounts WHERE name=$1"
+        const fetchVals = [name];
+        const result = await pool.query(fetchQuery, fetchVals);
+        const output = {
+            message: "Account updated successfully",
+            data: result.rows
+        } 
+        res.send(output);
+    } catch(err) {
+        console.error(err);
+        res.send("Server Error Occurred");
+    }
+})
+
 module.exports = router;
